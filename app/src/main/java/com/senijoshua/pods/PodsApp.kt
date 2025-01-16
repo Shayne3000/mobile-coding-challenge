7440d5fa7d76e3b785e4b1fe1f041d870ecd580b
Shayne3000 <alfred3000prime@hotmail.com>
2025-01-16 14:54:40 +0800

    Setup a coil image loader for the app

--- app/src/main/java/com/senijoshua/pods/PodsApp.kt
+++ app/src/main/java/com/senijoshua/pods/PodsApp.kt
@@ -1,8 +1,34 @@
 package com.senijoshua.pods
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PodsApp : Application() {
+class PodsApp : Application(), ImageLoaderFactory {
+
+    /**
+     * Setup a global image loader with specifics for memory and disk caching
+     * that disables respecting potential caching directives sent from remote
+     * image servers via cache-control headers.
+     */
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(this.cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .respectCacheHeaders(false)
+            .build()
+    }
 }
