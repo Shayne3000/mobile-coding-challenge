a469f4c05c7a1fb2f9084caeee1ae258842ddd59
Shayne3000 <alfred3000prime@hotmail.com>
2025-01-16 22:26:18 +0800

    Setup color, type, and theme

--- app/src/main/java/com/senijoshua/pods/presentation/theme/Color.kt
+++ app/src/main/java/com/senijoshua/pods/presentation/theme/Color.kt
@@ -2,10 +2,43 @@ package com.senijoshua.pods.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//// Light mode colors ////
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Toolbar
+val primaryLight = Color(0xFFFFFBFF)
+val onPrimaryLight = Color(0xFF1C1B1B)
+
+// List items
+val primaryContainerLight = Color(0xFFFFFFFF)
+val onPrimaryContainerLight = Color(0xFF313030)
+
+// Button
+val secondaryContainerLight = Color(0xFFDD2A4F)
+val onSecondaryContainerLight = Color(0xFFFFFFFF)
+
+// List item alternative
+val onTertiaryContainerLight = Color(0xFFC6C6C7)
+
+// Screen background
+val backgroundLight = Color(0xFFFFFBFF)
+
+// Error snack
+val errorLight = Color(0xFFBA1A1A)
+val onErrorLight = Color(0xFFFFFFFF)
+
+//// Dark mode colors ////
+val primaryDark = Color(0xFF201A19)
+val onPrimaryDark = Color(0xFFE5E2E1)
+
+val primaryContainerDark = Color(0xFF201A19)
+val onPrimaryContainerDark = Color(0xFFE5E2E1)
+
+val secondaryContainerDark = Color(0xFFD11F48)
+val onSecondaryContainerDark = Color(0xFFFFFFFF)
+
+val onTertiaryContainerDark = Color(0xFF5D5F5F)
+
+val backgroundDark = Color(0xFF201A19)
+
+val errorDark = Color(0xFFFFB4AB)
+val onErrorDark = Color(0xFF690005)
--- app/src/main/java/com/senijoshua/pods/presentation/theme/Type.kt
+++ app/src/main/java/com/senijoshua/pods/presentation/theme/Type.kt
@@ -3,32 +3,43 @@ package com.senijoshua.pods.presentation.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    // Toolbar style and Header title in detail screen
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 28.sp
     ),
-    labelSmall = TextStyle(
+
+    // Podcast card title
+    bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    // Author and Favourited text with favourited having italics as its style and detail screen text
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+
+    // Button text
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Light,
+        fontStyle = FontStyle.Italic,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
 )
