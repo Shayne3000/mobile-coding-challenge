36f3540b40557b63e22f0d1ff59a2efe7e07caf5
Shayne3000 <alfred3000prime@hotmail.com>
2025-01-17 22:19:44 +0800

    setup a progress bar component

---
+++ app/src/main/java/com/senijoshua/pods/presentation/components/PodsProgressIndicator.kt
@@ -0,0 +1,47 @@
+package com.senijoshua.pods.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import com.senijoshua.pods.R
+import com.senijoshua.pods.presentation.theme.PodsTheme
+
+const val PROGRESS_TAG: String = "PodsProgress"
+
+/**
+ * Reusable wrapper over [CircularProgressIndicator] with which an
+ * indeterminate progress bar of size, [size] can be setup
+ * in a given layout.
+ */
+@Composable
+fun PodsProgressIndicator(modifier: Modifier = Modifier, size: Dp) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .testTag(PROGRESS_TAG)
+                .size(size),
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
+
+@Composable
+@Preview(showSystemUi = true)
+private fun PodsProgressIndicatorPreview() {
+    PodsTheme {
+        PodsProgressIndicator(size = dimensionResource(id = R.dimen.progress_size_large))
+    }
+}
