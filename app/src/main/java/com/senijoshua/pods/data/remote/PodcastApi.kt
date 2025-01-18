7aea6c9f532e4c876468c5901be58e2f3bcc283f
Shayne3000 <alfred3000prime@hotmail.com>
2025-01-18 21:21:25 +0800

    refurbished the api interface endpoint function and added a proper response type

--- app/src/main/java/com/senijoshua/pods/data/remote/PodcastApi.kt
+++ app/src/main/java/com/senijoshua/pods/data/remote/PodcastApi.kt
@@ -1,11 +1,14 @@
 package com.senijoshua.pods.data.remote
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * Interface for interacting with the remote service
  */
 interface PodcastApi {
     @GET("/best_podcasts")
-    fun getBestPodcasts()
+    fun getBestPodcasts(
+        @Query("page") page: Int,
+    ): PodcastsResponse
 }
--- app/src/main/java/com/senijoshua/pods/data/remote/RemotePodcast.kt
+++ app/src/main/java/com/senijoshua/pods/data/remote/RemotePodcast.kt
@@ -1,8 +1,33 @@
 package com.senijoshua.pods.data.remote
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * Response model for the getBestPodcasts endpoint.
+ *
+ * NB: This represents how the response model would look
+ * in a non-dummy endpoint scenario.
+ */
+@Serializable
+data class PodcastsResponse(
+    @SerialName("previous_page_number")
+    val previousPageNumber: Int,
+    @SerialName("page_number")
+    val pageNumber: Int,
+    @SerialName("next_page_number")
+    val nextPageNumber: Int,
+    val podcasts: List<RemotePodcast>
+)
+
+/**
+ * Network model for a Podcast
+ */
+@Serializable
 data class RemotePodcast(
     val id: String,
     val title: String,
+    val thumbnail: String,
     val image: String,
     val publisher: String,
     val description: String,
