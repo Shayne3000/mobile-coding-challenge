7d0d3fef1218d97cf37dfbae2f4b03b934327688
Shayne3000 <alfred3000prime@hotmail.com>
2025-01-18 21:22:34 +0800

    added a global constants object and a mapper for podcasts models in various architectural layers

---
+++ app/src/main/java/com/senijoshua/pods/data/util/PodcastsMapper.kt
@@ -0,0 +1,51 @@
+package com.senijoshua.pods.data.util
+
+import com.senijoshua.pods.data.local.PodcastEntity
+import com.senijoshua.pods.data.remote.RemotePodcast
+import com.senijoshua.pods.presentation.detail.model.DetailPodcast
+import com.senijoshua.pods.presentation.home.model.HomePodcast
+
+/**
+ * Extension functions for converting data models to types relevant for the architectural
+ * layer in which they are used.
+ */
+
+/**
+ * Converts the network model to the local representation for persistence in the DB.
+ */
+fun RemotePodcast.toLocal() = PodcastEntity(
+    id = id,
+    title = title,
+    thumbnail = thumbnail,
+    image = image,
+    publisher = publisher,
+    description = description,
+    isFavourite = false,
+)
+
+fun List<RemotePodcast>.toLocal() = map(RemotePodcast::toLocal)
+
+/**
+ * Converts the local model to the model relevant for the home screen in the presentation layer.
+ */
+fun PodcastEntity.toHomePodcast() = HomePodcast(
+    id = id,
+    title = title,
+    thumbnail = thumbnail,
+    publisher = publisher,
+    isFavourite = isFavourite
+)
+
+fun List<PodcastEntity>.toHomePodcast() = map(PodcastEntity::toHomePodcast)
+
+/**
+ * Converts the local model to the model relevant for the detail screen in the presentation layer.
+ */
+fun PodcastEntity.toDetailPodcast() = DetailPodcast(
+    id = id,
+    title = title,
+    image = image,
+    publisher = publisher,
+    description = description,
+    isFavourite = isFavourite
+)
---
+++ app/src/main/java/com/senijoshua/pods/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.senijoshua.pods.util
+
+/**
+ * App-level constants
+ */
+object Constants {
+    const val MAX_PODCASTS_PER_PAGE = 10
+}
