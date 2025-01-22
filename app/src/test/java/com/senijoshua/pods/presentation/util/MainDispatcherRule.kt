95bd5d2b82a80111d243a2ed76ce4a5abea40a7a
Shayne3000 <alfred3000prime@hotmail.com>
2025-01-22 17:52:38 +0800

    Added test rules for the dispatcher and saved state handle

---
+++ app/src/test/java/com/senijoshua/pods/presentation/util/MainDispatcherRule.kt
@@ -0,0 +1,28 @@
+package com.senijoshua.pods.presentation.util
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+ * Test rule for overriding the main dispatcher used by scopes
+ * like the viewModelScope with a test dispatcher for use in all
+ * test cases within a Test class.
+ */
+@ExperimentalCoroutinesApi
+class MainDispatcherRule(
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
---
+++ app/src/test/java/com/senijoshua/pods/presentation/util/SavedStateHandleRule.kt
@@ -0,0 +1,36 @@
+package com.senijoshua.pods.presentation.util
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.internalToRoute
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+ * Work-around Test rule for mocking [SavedStateHandle] so as to side-step its
+ * dependency on the Bundle Android API that is internally employed
+ * when passing arguments via a navigation route with [SavedStateHandle].
+ *
+ * This rule negates the need to use Robolectric to test ViewModels
+ * that employ a [SavedStateHandle] for accessing route arguments which
+ * would effectively make it an instrumented test.
+ */
+class SavedStateHandleRule(
+    private val route: Any,
+) : TestWatcher() {
+    val savedStateHandleMock: SavedStateHandle = mockk()
+
+    override fun starting(description: Description) {
+        mockkStatic("androidx.navigation.SavedStateHandleKt")
+        every { savedStateHandleMock.internalToRoute<Any>(any(), any()) } returns route
+        super.starting(description)
+    }
+
+    override fun finished(description: Description) {
+        unmockkStatic("androidx.navigation.SavedStateHandleKt")
+        super.finished(description)
+    }
+}
